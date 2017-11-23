cd688cc748f0d39bfa496c63d33faf281c04ac9e
fernandoMontes <fernando@element.in>
2017-11-23 22:44:13 +0100

    - fixed initialization problem

--- src/main/java/com/ferzerkerx/lucenedemo/LuceneDemoApplication.java
+++ src/main/java/com/ferzerkerx/lucenedemo/LuceneDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LuceneDemoApplication {
 
-    private LuceneDemoApplication() {
+    public LuceneDemoApplication() {
     }
 
     public static void main(String[] args) {
